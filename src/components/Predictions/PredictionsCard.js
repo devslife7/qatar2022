e036603dd45e1904f8268f099b94f8d910dc855a
Marcos Velasco <devslife7@gmail.com>
2022-11-18 21:22:49 -0500

    added countryCodes

--- src/components/Predictions/PredictionsCard.js
+++ src/components/Predictions/PredictionsCard.js
@@ -4,14 +4,14 @@ import Grid from '@mui/material/Grid'
 export default function PredictionsCard({ user }) {
   return (
     <>
-      <Grid item container xs={6} justifyContent='center' style={{ padding: 0 }}>
+      <Grid item container xs={6} justifyContent='flex-start' style={{ padding: '0 0 0 10px' }}>
         <Grid item>{user.first_name + ' ' + user.last_name}</Grid>
       </Grid>
-      <Grid item container xs={3} justifyContent='center' style={{ padding: 0 }}>
+      <Grid item container xs={3} justifyContent='flex-end' style={{ padding: 0 }}>
         <Grid>0/48</Grid>
       </Grid>
-      <Grid item container xs={3} justifyContent='center' style={{ padding: 0 }}>
-        <Grid style={{ padding: 0 }}>{user.predictionsGS}</Grid>
+      <Grid item container xs={3} justifyContent='flex-end' style={{ padding: 0 }}>
+        <Grid style={{ padding: 0 }}>0%</Grid>
       </Grid>
     </>
   )
