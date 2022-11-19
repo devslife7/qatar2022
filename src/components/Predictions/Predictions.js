2640f431280495aa808174c9e344f1caa4a3af5b
Marcos Velasco <devslife7@gmail.com>
2022-11-18 20:13:26 -0500

    added env file and got user fetch working

--- src/components/Predictions/Predictions.js
+++ src/components/Predictions/Predictions.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Predictions.css'
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import PredictionsCard from './PredictionsCard'
 
+// const serverURL = process.env.REACT_APP_SERVER_URL
+const serverURL = 'http://localhost:3000'
+
 export default function Predictions() {
+  const [userData, setUserData] = useState([])
+  useEffect(() => {
+    fetch(serverURL + '/users')
+      .then(resp => resp.json())
+      .then(data => setUserData(data.users))
+      .catch(err => console.log(err))
+  }, [])
+
   const userPredictions = () => {
-    // return list.map((user, idx) => {
-    //   return <PredictionsCard key={idx} user={user} />
-    // })
-    return <div>Predictions Placeholder</div>
+    console.log(userData.users)
+    return userData.map((user, idx) => {
+      return <PredictionsCard key={idx} user={user} />
+    })
   }
 
   return (
@@ -18,8 +29,8 @@ export default function Predictions() {
         <h2 className='predictions__title'>Predictions</h2>
 
         <Paper>
-          <Grid container spacing={2}>
-            <div className='predictions__section'>{userPredictions()}</div>
+          <Grid container spacing={2} className='predictions_containerGrid' style={{ padding: 10 }}>
+            {userPredictions()}
           </Grid>
         </Paper>
       </div>
--- src/components/Predictions/PredictionsCard.js
+++ src/components/Predictions/PredictionsCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
 
-export default function PredictionsCard() {
+export default function PredictionsCard({ user }) {
   return (
     <>
-      <Grid item xs={4}>
-        Name
+      <Grid item container xs={6} justifyContent='center' style={{ padding: 0 }}>
+        <Grid item>{user.first_name + ' ' + user.last_name}</Grid>
       </Grid>
-      <Grid item xs={4}>
-        score
+      <Grid item container xs={3} justifyContent='center' style={{ padding: 0 }}>
+        <Grid>0/48</Grid>
       </Grid>
-      <Grid item xs={4}>
-        precentage
+      <Grid item container xs={3} justifyContent='center' style={{ padding: 0 }}>
+        <Grid style={{ padding: 0 }}>{user.predictionsGS}</Grid>
       </Grid>
     </>
   )
