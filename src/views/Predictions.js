bcdd7860037e39a6db81e0f5d57032f5df59832b
Marcos Velasco <devslife7@gmail.com>
2022-11-23 02:37:43 -0500

    enabled live data on refresh

--- src/views/Predictions.js
+++ src/views/Predictions.js
@@ -6,12 +6,79 @@ import PredictionsCard from '../assets/PredictionsCard'
 // import TextField from '@mui/material/TextField'
 
 const serverURL = process.env.REACT_APP_SERVER_URL
-const officialFixtureResult = ['A2', 'B1', 'A4', 'TIE', 'C2', 'TIE', 'TIE', 'D1']
 
-export default function Predictions() {
+const resultsIdCodesKey = {
+  1569: 'A1',
+  2382: 'A2',
+  13: 'A3',
+  1118: 'A4',
+
+  10: 'B1',
+  22: 'B2',
+  2384: 'B3',
+  767: 'B4',
+
+  26: 'C1',
+  23: 'C2',
+  16: 'C3',
+  24: 'C4',
+
+  2: 'D1',
+  20: 'D2',
+  21: 'D3',
+  28: 'D4',
+
+  9: 'E1',
+  29: 'E2',
+  25: 'E3',
+  12: 'E4',
+
+  1: 'F1',
+  5529: 'F2',
+  31: 'F3',
+  3: 'F4',
+
+  6: 'G1',
+  14: 'G2',
+  15: 'G3',
+  1530: 'G4',
+
+  27: 'H1',
+  1504: 'H2',
+  7: 'H3',
+  17: 'H4',
+
+  TIE: 'TIE',
+}
+
+export default function Predictions({ fixtures }) {
   const [userData, setUserData] = useState([])
   // const [searchTerm, setSearchTerm] = useState('')
   const pricePool = userData.length * 50
+  // const officialFixtureResult = ['A2', 'B1', 'A4', 'TIE', 'C2', 'TIE', 'TIE', 'D1']
+
+  let resultArr = []
+  let resultArr2 = []
+  fixtures.map(fixture => {
+    if (fixture.fixture.status.short === 'FT') {
+      if (fixture.goals.home === fixture.goals.away) {
+        resultArr.push('TIE')
+      } else if (fixture.goals.home > fixture.goals.away) {
+        resultArr.push(fixture.teams.home.id)
+      } else {
+        resultArr.push(fixture.teams.away.id)
+      }
+    }
+  })
+
+  console.log(resultArr)
+  resultArr.map(winnerId => {
+    resultArr2.push(resultsIdCodesKey[winnerId])
+  })
+  console.log(resultArr2)
+
+  // Get official results Array
+  const officialFixtureResult = resultArr2
 
   useEffect(() => {
     fetch(serverURL + '/users')
@@ -50,6 +117,7 @@ export default function Predictions() {
 
   // finds correct predicted matches of user
   const findMatches = (officialResult = [], predictions = []) => {
+    if (officialResult.length === 0) return
     let counter = 0
     for (let i = 0; i < officialResult.length; i++) {
       officialResult[i] === predictions[i] && counter++
