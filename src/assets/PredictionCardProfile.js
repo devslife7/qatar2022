ed945c9af0ec151f93f9d0de70da98d398fb38ec
Marcos Velasco <devslife7@gmail.com>
2022-11-29 03:54:31 -0500

    added flags to guesses

--- src/assets/PredictionCardProfile.js
+++ src/assets/PredictionCardProfile.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import '../styles/Profile.css'
-import { countryCodes } from '../helpers/Data'
+import { countryCodesShort } from '../helpers/Data'
 
 export default function PredictionCardProfile({ fixture, userPrediction }) {
   console.log('Fixture: ', fixture)
   // console.log('Fixture: ', countryCodes[userPrediction])
+  const countryFlag = countryCodesShort[`${userPrediction + 'Flag'}`]
   return (
     <div className='predictionCardProfile'>
-      <div className='profile__predictions'>{countryCodes[userPrediction]}</div>
+      <div className='profile__predictions'>
+        <div>{countryCodesShort[userPrediction]}</div>
+        <div>
+          <img src={countryFlag} className='profile__flags' />
+        </div>
+      </div>
       <div className='profile__fixtures'>
-        <div>{fixture.teams.home.name}</div>
+        <div style={{ backgroundColor: 'green', opacity: '0.7' }}>{fixture.teams.home.name}</div>
         <img src={fixture.teams.home.logo} className='profile__flags' />
         <div>vs</div>
         <img src={fixture.teams.away.logo} className='profile__flags' />
