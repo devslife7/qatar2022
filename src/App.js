3c53430226e00314e126707386d43b27ab0cf314
Marcos Velasco <devslife7@gmail.com>
2022-12-05 15:57:33 -0500

    moved navbar and footer to layouts folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Footer from './components/Footer'
+import Footer from './layouts/Footer'
 import MainPage from './pages/MainPage'
 import Profile from './pages/Profile'
 import PredictionsOct from './pages/PredictionsOct'
